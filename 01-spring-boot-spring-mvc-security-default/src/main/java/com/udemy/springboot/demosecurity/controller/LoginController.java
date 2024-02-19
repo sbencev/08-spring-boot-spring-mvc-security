9e7e8988b68b4f7b22fa988a395356b804ac4dac
sbencev <112954344+sbencev@users.noreply.github.com>
2024-02-19 15:43:47 +0100

    Initial commit

---
+++ 01-spring-boot-spring-mvc-security-default/src/main/java/com/udemy/springboot/demosecurity/controller/LoginController.java
@@ -0,0 +1,23 @@
+package com.udemy.springboot.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/showMyLoginPage")
+    public String showMyLoginPage(){
+
+        return "fancy-login";
+
+    }
+
+    @GetMapping("/access-denied")
+    public String showAccessDenied(){
+
+        return "access-denied";
+
+    }
+
+}
